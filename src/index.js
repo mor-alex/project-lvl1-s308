abb8f8e4d823180c77bae72f46ca8c56c82b976e
Alexey Morozov <alexmorozov788@gmail.com>
2018-07-09 16:24:41 +0300

    Greeting for user

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    console.log('Welcome to the Brain Games!\n');
+    const getName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${getName}!\n`);
+};
