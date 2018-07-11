be6c0c80242d28f399c9cc6f9157f950179ae3e7
Alexey Morozov <alexmorozov788@gmail.com>
2018-07-11 09:28:03 +0300

    Fix brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,17 +4,13 @@ const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) +
 
 const isEven = num => num % 2 === 0;
 
-const happyend = name => console.log(`Congratulations, ${name}!\n`);
-
-const badend = name => console.log(`Let's try again, ${name}!\n`);
-
 export default () => {
   console.log('Welcome to the Brain Games!\n');
   console.log('Answer "yes" if number odd otherwise answer "no".\n');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
-  const runEvenGame = (acc, numOfSucAttempts) => {
+  const runEvenGame = (numOfSucAttempts, acc = 1) => {
     const number = getRandomInt(1, 100);
     console.log('Question: ', number);
     const correctAnswer = isEven(number) ? 'yes' : 'no';
@@ -22,12 +18,14 @@ export default () => {
     if (userAnswer === correctAnswer) { console.log('Correct!'); }
     if (userAnswer !== correctAnswer) {
       console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      return badend;
+      console.log(`Let's try again, ${userName}!\n`);
+      return;
     }
     if (acc === numOfSucAttempts) {
-      return happyend(userName);
+      console.log(`Congratulations, ${userName}!\n`);
+      return;
     }
-    return runEvenGame(acc + 1, numOfSucAttempts);
+    runEvenGame(numOfSucAttempts, acc + 1);
   };
-  runEvenGame(1, 3);
+  runEvenGame(3);
 };
