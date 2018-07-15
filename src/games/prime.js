d1de3e8d017969b11106a5ae16344a6805ca971e
Alexey Morozov <alexmorozov788@gmail.com>
2018-07-15 16:27:25 +0300

    refactor(prime.js) fix isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,10 +3,10 @@ import flow from '../flow';
 import getRandomInt from '../utils';
 
 const isPrime = (num) => {
-  const n = Math.sqrt(num);
   if (num < 2) {
     return false;
   }
+  const n = Math.sqrt(num);
   for (let i = 2; i <= n; i += 1) {
     if (num % i === 0) {
       return false;
